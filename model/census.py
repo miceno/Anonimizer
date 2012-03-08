8416597e5c764c9f3a6299c05dfeace4ef5084eb
miceno <orestes@acm.org>
2012-03-09 00:01:13 +0100

    Census class and tests

--- model/census.py
+++ model/census.py
@@ -12,13 +12,38 @@ import os
 import unittest
 
 
-class census:
-    def __init__(self):
+
+class Census:
+    def __init__(self, domain, address_seq ):
+        self.domain = domain
+        self.address_seq = address_seq
+        self.census = {}
+        for a in self.address_seq:
+            user = AnonymousUser( a, self.domain)
+            self.census[ user.id ] = user
+        pass
+        
+    def verify( self, uuid ):
+        return 
+
+
+class CensusTests(unittest.TestCase):
+    def setUp(self):
+        self.emails = [ "first@first.com", "second@second.com", "third@third.com"]
+        self.domain = "domain.com"
+        pass
+        
+    def test_domain( self ):
+        emptySeq = []
+        c = Census( self.domain, emptySeq )
+        self.assertEqual( c.domain, self.domain )
+        self.assertSequenceEqual( c.address_seq, emptySeq )
         pass
 
-
-class censusTests(unittest.TestCase):
-    def setUp(self):
+    def test_emails( self ):
+        c = Census( self.domain, self.emails )
+        self.assertEqual( c.domain, self.domain )
+        self.assertSequenceEqual( c.address_seq, self.emails )
         pass
 
 
